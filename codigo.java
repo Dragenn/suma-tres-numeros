8e4d829518e1bbaff17b139d236a0dbec8530a1e
Dragenn <108434801+Dragenn@users.noreply.github.com>
2023-03-20 12:24:07 -0400

    Create codigo.java

---
+++ codigo.java
@@ -0,0 +1,11 @@
+public class SumaTresNumeros {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 10;
+        int num3 = 15;
+        
+        int resultado = num1 + num2 + num3;
+        
+        System.out.println("El resultado de la suma es: " + resultado);
+    }
+}
